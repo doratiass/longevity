999b25cd6bcc06efe40766abd72a78973cc4c7d5
doratiass <doratiass@egmail.com>
2023-12-21 08:53:00 +0200

    add longevity shap

---
+++ scripts/save_files.R
@@ -0,0 +1,7 @@
+save(cal_plot,ctl_grid,df_split,df_test,
+     df_train,df_train_cv,final_lasso,final_lasso_fit,
+     final_xgb,final_xgb_fit,lambda_grid,lasso_best_auc,
+     lasso_prep,lasso_rec,lasso_res,lasso_spec,
+     lasso_wf,ml_df,thresh_corr,thresh_other,
+     xgb_bake,xgb_best_auc,xgb_grid,xgb_prep,
+     xgb_rec,xgb_res,xgb_spec,xgb_wf, file = "raw_data/models.RData")
