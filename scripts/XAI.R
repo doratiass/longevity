41f7c9b25994a632cfa7a8e23bcbe3d6d0e863d9
doratiass <53535466+doratiass@users.noreply.github.com>
2023-12-27 16:57:05 +0200

    add 3 models plot

--- scripts/XAI.R
+++ scripts/XAI.R
@@ -10,12 +10,14 @@ library(probably)
 library(gridExtra)
 library(doFuture)
 library(ggpubr)
-library(ggVennDiagram)
+library(ggbump)
 tidymodels_prefer()
 
 source("~/Documents/stat_projects/longevity/longevity_shap/dict.R")
 set.seed(45)
 cat("\f")
+
+var_num <- 10
 
 pfun <- function(object, newdata) {  # needs to return a numeric vector
   predict(object, newdata, type = "prob")[,".pred_centenarian",drop = T]
@@ -28,7 +30,7 @@ shap_exp_log <- fastshap::explain(extract_workflow(final_log_fit), X = df_test,
 shap_log <- shapviz(shap_exp_log)
 
 shap_imp_log <- sv_importance(shap_log, kind = "bar", show_numbers = TRUE,
-                              max_display = 20) +
+                              max_display = var_num) +
   scale_y_discrete(labels = vars_label) +
   theme_classic()
 
@@ -39,7 +41,7 @@ shap_exp_lasso <- fastshap::explain(extract_workflow(final_lasso_fit), X = df_te
 shap_lasso <- shapviz(shap_exp_lasso)
 
 shap_imp_lasso <- sv_importance(shap_lasso, kind = "bar", show_numbers = TRUE,
-                                max_display = 20) +
+                                max_display = var_num) +
   scale_y_discrete(labels = vars_label) +
   theme_classic()
 
@@ -59,13 +61,13 @@ shap_xgb <- shapviz(extract_fit_engine(final_xgb_fit), X_pred = xgb_shap_data,
                     interactions = TRUE)
 
 shap_imp_bar_xgb <- sv_importance(shap_xgb, kind = "bar", show_numbers = TRUE,
-                                  max_display = 20) +
+                                  max_display = var_num) +
   scale_y_discrete(labels = vars_label) +
   theme_classic(base_size = 16)
 
 
 shap_imp_bee_xgb <- sv_importance(shap_xgb, kind = "beeswarm", show_numbers = FALSE,
-                                  max_display = 20) +
+                                  max_display = var_num) +
   scale_y_discrete(labels = vars_label) +
   theme_classic(base_size = 16)
 
@@ -104,16 +106,38 @@ dep_plot %>%
   facet_wrap(~feature, scales = "free") +
   theme_classic()
 
-# Venn Diagram ####
-library(RColorBrewer)
-myCol <- brewer.pal(3, "Pastel2")
+# 3 models vars ####
 log_shap_vars <- as.character(unique(sapply(shap_imp_log$data$feature, label_get)))
 lasso_shap_vars <- as.character(unique(sapply(shap_imp_lasso$data$feature, label_get)))
 xgb_shap_vars <- as.character(unique(sapply(shap_imp_bar_xgb$data$feature, label_get)))
+xgb_shap_vars[xgb_shap_vars == "Smoking Status (more than 20 cigarretes per day)"] <- "Smoking Status"
+xgb_shap_vars[xgb_shap_vars == "Smoking Status (11-20 cigarretes per day)"] <- "Smoking Status"
+xgb_shap_vars[xgb_shap_vars == "Smoking Status (never smoked)"] <- "Smoking Status"
+xgb_shap_vars <- unique(xgb_shap_vars)
 
-intersect(log_shap_vars,lasso_shap_vars)
-intersect(log_shap_vars,xgb_shap_vars)
-intersect(lasso_shap_vars,xgb_shap_vars)
-intersect(log_shap_vars,intersect(lasso_shap_vars,xgb_shap_vars))
-ggVennDiagram(
-  x = list("Log" = log_shap_vars, "LASSO" = lasso_shap_vars, "XGB" = xgb_shap_vars))
+all <- intersect(log_shap_vars,intersect(lasso_shap_vars,xgb_shap_vars))
+log_lasso <- intersect(log_shap_vars,lasso_shap_vars)[!(intersect(log_shap_vars,lasso_shap_vars) %in% all)]
+log_xgb <- intersect(log_shap_vars,xgb_shap_vars)[!(intersect(log_shap_vars,xgb_shap_vars) %in% all)]
+xgb_lasso <- intersect(xgb_shap_vars,lasso_shap_vars)[!(intersect(xgb_shap_vars,lasso_shap_vars) %in% all)]
+
+tibble(
+  log = log_shap_vars,
+  lasso = lasso_shap_vars,
+  xgb = c(xgb_shap_vars,NA)
+) %>%
+  pivot_longer(cols = c(log,lasso,xgb),
+               names_to = "model",
+               values_to = "var") %>%
+  drop_na() %>%
+  mutate(model = factor(model, levels = c("log", "lasso", "xgb"))) %>%
+  arrange(var) %>%
+  ggplot(aes(x = model, y = var, color = var, group = var)) +
+  geom_point(size = 3) +
+  geom_line(size = 1.5) +
+  labs(x = "",
+       y = "") +
+  theme_classic(base_size = 16)+
+  theme(
+    legend.position = "none"
+  )
+  
