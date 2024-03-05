51e406f65ddd2bf5f65617b352690fb88420a681
doratiass <53535466+doratiass@users.noreply.github.com>
2024-03-05 15:14:23 +0200

    update

--- scripts/XAI.R
+++ scripts/XAI.R
@@ -40,8 +40,7 @@ step_model$anova %>%
 
 ## LASSO ----------------------------------------------------------------------
 train_model <- lasso_train_fit
-#all_cores <- parallel::detectCores(logical = TRUE)
-all_cores <- 4
+all_cores <- parallel::detectCores(logical = TRUE)
 cl <- makeCluster(all_cores-2)
 registerDoParallel(cl)
 
